fd91a3379574662ecf867f5b2899081c28ee97d2
Job Vulkers <job.vulkers@gmail.com>
2019-09-09 14:16:50 +0200

    pizzahut

---
+++ create-database.sql
@@ -0,0 +1,9 @@
+/*Database creëren*/
+CREATE DATABASE kastje;
+/*Gebruiker aanmaken*/
+CREATE USER 'backend-service'@'localhost' IDENTIFIED BY 'geheim!';
+/*Permissies geven aan reeds aangemaakte gebruiker*/
+GRANT SELECT ON kastje.* TO 'backend-service'@'localhost';
+USE kastje;
+/*Table creëren*/
+CREATE TABLE IF NOT EXISTS history (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, tijd TIMESTAMP);
