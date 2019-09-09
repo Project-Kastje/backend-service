3c08adfa02a8d6ca48a139ce138d818170c78c08
hypothermic <admin@hypothermic.nl>
2019-09-09 14:27:38 +0200

    Grant insert on * to backend-service

--- create-database.sql
+++ create-database.sql
@@ -4,6 +4,7 @@ CREATE DATABASE kastje;
 CREATE USER 'backend-service'@'localhost' IDENTIFIED BY 'geheim!';
 /*Permissies geven aan reeds aangemaakte gebruiker*/
 GRANT SELECT ON kastje.* TO 'backend-service'@'localhost';
+GRANT INSERT on kastje.* TO 'backend-service'@'localhost';
 USE kastje;
 /*Table creëren*/
 CREATE TABLE IF NOT EXISTS history (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, tijd TIMESTAMP);
