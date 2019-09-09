f2820489a4f17632a24d9545bed596a90347597c
hypothermic <admin@hypothermic.nl>
2019-09-09 15:04:39 +0200

    Check if root

--- service.py
+++ service.py
@@ -1,5 +1,6 @@
 from sense_hat         import SenseHat
 from time              import sleep
+import os
 import mysql.connector as mariadb
 
 # --- configuratie
@@ -27,6 +28,17 @@ def iterate():
     #if x > 1 or y > 1 or z > 1:
     if x > 0.15 or y > 0.15:
         print("INDRINGER")
+
+f = open("backend-service.log", "a")
+f.write("Start service")
+
+if os.geteuid() != 0:
+    f.write("ROOT NODIG!!!!!")
+    f.close()
+    exit("Root is nodig voor het uitvoeren van dit script!")
+
+f.write("OK")
+f.close()
 
 try:
     while True:
