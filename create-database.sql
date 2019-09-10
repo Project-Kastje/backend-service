dc3399e58b2366fe2f796414659e1e3c9512682c
jeroenstek <55084629+jeroenstek@users.noreply.github.com>
2019-09-10 12:08:47 +0200

    Update create-database.sql

--- create-database.sql
+++ create-database.sql
@@ -3,8 +3,7 @@ CREATE DATABASE kastje;
 /*Gebruiker aanmaken*/
 CREATE USER 'backend-service'@'localhost' IDENTIFIED BY 'geheim!';
 /*Permissies geven aan reeds aangemaakte gebruiker*/
-GRANT SELECT ON kastje.* TO 'backend-service'@'localhost';
-GRANT INSERT on kastje.* TO 'backend-service'@'localhost';
+GRANT ALL ON kastje.* TO 'backend-service'@'localhost';
 USE kastje;
 /*Table creëren*/
 CREATE TABLE IF NOT EXISTS history (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,tijd DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);
