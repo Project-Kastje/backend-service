58286a4a2565ab5efdb1d61c4dec36b3efadc659
JobVulkers <55084678+JobVulkers@users.noreply.github.com>
2019-09-13 11:39:35 +0200

    fd

--- service.py
+++ service.py
@@ -48,7 +48,7 @@ def iterate():
     y = abs(accel['x'])
     z = abs(accel['z'])
     #if x > 1 or y > 1 or z > 1:
-    if x > 0.5 or y > 0.5:
+    if x > 0.15 or y > 0.15:
         print("INDRINGER")
         alarm()
 
