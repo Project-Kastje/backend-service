ea1c4ff084ce34b714a351fe92328d357e1de0c3
DerpLvlAsian <ronaldbijsma@gmail.com>
2019-09-13 11:02:25 +0200

    yeetalarmtabledb

--- create-database.sql
+++ create-database.sql
@@ -7,3 +7,5 @@ GRANT ALL PRIVILEGES ON kastje.* TO 'backend-service'@'localhost';
 USE kastje;
 /*Correct table*/
 CREATE TABLE kastje.history ( id INT(22) NOT NULL AUTO_INCREMENT , tijd TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (id));
+CREATE TABLE kastje.alarm (stat INT(1));
+INSERT INTO kastje.alarm (stat) VALUES (1);
